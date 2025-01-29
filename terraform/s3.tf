617b78462f8ec6fdeba347ba62328eb46650b506
Albert Ribas <albert.ribas@flixbus.com>
2025-01-29 15:33:43 +0100

    Move tf common tags to locals file

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,10 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 
-  tags = {
-    Environment            = local.env
-    Product                = local.service_name
-    Terraform              = "true"
-    "Terraform Repository" = var.repository
-  }
+  tags = local.tf_common_tags
 }
